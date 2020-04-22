e944c9f7a163fe5715e7237736a907e58dbfc4d7
Your NameRajkumar <theenanraj@gmail.com>
2020-04-22 22:33:57 +0530

    homepage splitted into components and styling done

--- onlineshop/src/App.js
+++ onlineshop/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage  from "./homepPage"
+import HomePage  from './pages/homePage'
 import './App.css';
 
 function App() {
---
+++ onlineshop/src/pages/homePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../components/directory/directory.jsx'
+import "./homePage.scss"
+
+const Homepage = () => (
+    <div className="homepage">
+      <Directory />
+    </div>
+)
+
+export default Homepage
